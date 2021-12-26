f488fc1d11d0d913af1d172d2ffce06b25398191
J Sokol <jan.sokol.glx@gmail.com>
2021-12-26 22:33:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+
+
+```
+sudo fdisk -l
+    sudo fdisk /dev/sdb
+    mkfs.ext4 /dev/sdb1
+    sudo mkfs.ext4 /dev/sdb1
+```
---
+++ roles/bitcoin-status-scripts/files/get_status.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+bold=$(tput bold); \
+red=`tput setaf 1`; \
+green=`tput setaf 2`; \
+yellow=`tput setaf 3`; \
+normal=$(tput sgr0); \
+
+
+
+echo $tot
+
+#R1=$(cat /sys/class/net/wlan1/statistics/rx_bytes)
+#T1=$(cat /sys/class/net/wlan1/statistics/tx_bytes)
+#R2=$(cat /sys/class/net/wlan1/statistics/rx_bytes)
+#T2=$(cat /sys/class/net/wlan1/statistics/tx_bytes)
+#totalnw=$(( (R2 + T2 - R1 - T1) / 1024 ))
+
+
+while true; do \
+	prevblocks=$blocks
+	bestblockhash=`bitcoin-cli -datadir=/mnt/bitcoin getbestblockhash`; \
+	blockts=`bitcoin-cli -datadir=/mnt/bitcoin getblock $bestblockhash | jq .time`; \
+	blockdate=`date --date="@$blockts" "+%Y/%m/%d-%H:%M:%S"`; \
+	btcprice=`curl -s "https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=USD&ts=$blockts" | jq .BTC.USD`; \
+	currentdate=`date "+%Y/%m/%d-%H:%M:%S"`; \
+	blocks=`bitcoin-cli -datadir=/mnt/bitcoin getblockcount`; \
+	diskfilled=`df -h|grep bitcoin|tr -s " " |cut -d" " -f3`; \
+	diskleft=`df -h|grep bitcoin|tr -s " " |cut -d" " -f4`; \
+	diskall=`df -h|grep bitcoin|tr -s " " |cut -d" " -f2`
+	blockdiff=$((blocks - prevblocks))
+	printf "\n${currentdate} || disk: ${red}${diskfilled}${normal}/$diskall ($diskleft left) | block: ${green}$blocks${normal} (${yellow}+${blockdiff}${normal}, ${blockdate}) | ${bold}$btcprice${normal} BTC/USD" ; \
+	sleep 10; \
+done
