d91f92819a8bc4f2e9c073a4f0bff8db7f3d7123
J Sokol <jan.sokol.glx@gmail.com>
2021-12-27 13:30:45 +0100

    Commit

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 ```
 sudo fdisk -l
-    sudo fdisk /dev/sdb
-    mkfs.ext4 /dev/sdb1
-    sudo mkfs.ext4 /dev/sdb1
+sudo fdisk /dev/sdb
+mkfs.ext4 /dev/sdb1
+sudo mkfs.ext4 /dev/sdb1
+sudo e2label /dev/sdb1 bitcoin
 ```
--- roles/bitcoin-status-scripts/files/get_status.sh
+++ roles/bitcoin-status-scripts/files/get_status.sh
@@ -6,9 +6,12 @@ green=`tput setaf 2`; \
 yellow=`tput setaf 3`; \
 normal=$(tput sgr0); \
 
-
-
+DATADIR="{{ bitcoind_data_dir }}"
+RPCPASSWORD="{{ bitcoind_rpc_pass }}"
+RPCUSER="{{ bitcoind_rpc_user }}"
 echo $tot
+
+
 
 #R1=$(cat /sys/class/net/wlan1/statistics/rx_bytes)
 #T1=$(cat /sys/class/net/wlan1/statistics/tx_bytes)
@@ -19,12 +22,12 @@ echo $tot
 
 while true; do \
 	prevblocks=$blocks
-	bestblockhash=`bitcoin-cli -datadir=/mnt/bitcoin getbestblockhash`; \
-	blockts=`bitcoin-cli -datadir=/mnt/bitcoin getblock $bestblockhash | jq .time`; \
+	bestblockhash=`bitcoin-cli -datadir="$DATADIR" -rpcpassword="$RPCPASSWORD" -rpcuser="$RPCUSER" getbestblockhash`; \
+	blockts=`bitcoin-cli -datadir="$DATADIR" -rpcpassword="$RPCPASSWORD" -rpcuser="$RPCUSER" getblock $bestblockhash | jq .time`; \
 	blockdate=`date --date="@$blockts" "+%Y/%m/%d-%H:%M:%S"`; \
 	btcprice=`curl -s "https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=USD&ts=$blockts" | jq .BTC.USD`; \
 	currentdate=`date "+%Y/%m/%d-%H:%M:%S"`; \
-	blocks=`bitcoin-cli -datadir=/mnt/bitcoin getblockcount`; \
+	blocks=`bitcoin-cli -datadir="$DATADIR" -rpcpassword="$RPCPASSWORD" -rpcuser="$RPCUSER" getblockcount`; \
 	diskfilled=`df -h|grep bitcoin|tr -s " " |cut -d" " -f3`; \
 	diskleft=`df -h|grep bitcoin|tr -s " " |cut -d" " -f4`; \
 	diskall=`df -h|grep bitcoin|tr -s " " |cut -d" " -f2`
